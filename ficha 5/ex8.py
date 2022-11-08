760b6e60a47740aeea2faad6d741e9c4d1935d0f
Beatriz Lisboa <btrzlisboa037@gmail.com>
2022-11-08 19:17:33 +0000

    8

--- ficha 5/ex8.py
+++ ficha 5/ex8.py
@@ -1,7 +1,16 @@
 def replaceNumbers(texto):
-    if texto.find("1"):
-        texto.replace ("1","um")
-    elif texto.find("2"):
-        texto.replace("2", "dois") 
-    elif texto.find("2"):
-        texto.replace("2", "dois")
+    texto = texto.replace ("1","um")
+    texto = texto.replace("2", "dois") 
+    texto = texto.replace("3", "três")
+    texto = texto.replace("4", "quatro")
+    texto = texto.replace("5", "cinco")
+    texto = texto.replace("6", "seis")
+    texto = texto.replace("7", "sete")
+    texto = texto.replace("8", "oito")
+    texto = texto.replace("9", "nove")
+    texto = texto.replace("0", "zero")
+    print(texto)
+
+
+texto = input("Insira um texto: ")
+replaceNumbers(texto)
